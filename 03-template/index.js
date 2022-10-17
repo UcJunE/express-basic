0682d6cc32b3d85757638f669c1a51bf709463fd
JunE <ucjune90@gmail.com>
2022-10-17 14:18:04 +0800

    Lesson about template

--- 03-template/index.js
+++ 03-template/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "hbs");
+//all static file(ie images , css, js  will be located inside the 'public' folder);
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("index.hbs");
@@ -10,13 +12,12 @@ app.get("/", function (req, res) {
 app.get("/greeting/:firstName/:lastName", function (req, res) {
   let firstName = req.params.firstName;
   let lastName = req.params.lastName;
-  res.render("greet",{
-    "firstName":firstName,
-    "lastName":lastName
+  res.render("greet", {
+    firstName: firstName,
+    lastName: lastName,
   });
 });
 
 app.listen(3000, function () {
   console.log("server has started at port 3000");
 });
-
